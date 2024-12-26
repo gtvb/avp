ce258ef4520109e931edadfaae566225aaacbb1d
gtvb <gabriel.tiso@proton.me>
2024-12-26 11:20:16 -0300

    feat: refactored media class to facilitate future operations

--- media.c
+++ media.c
@@ -1,75 +1,102 @@
 #include "media.h"
 
-Media *media_alloc(int video_area_width, int video_area_height) {
+Media *media_alloc() {
   Media *media = (Media *)malloc(sizeof(Media));
 
   media->filename = (char *)malloc(sizeof(char) * MAX_FILEPATH_SIZE);
+  if (!media->filename)
+    return NULL;
+
   media->fmt_ctx = NULL;
+  media->nb_streams = 0;
 
-  media->video_stream_index = -1;
   media->audio_stream_index = -1;
-
-  media->audio_codec = NULL;
-  media->video_codec = NULL;
+  media->video_stream_index = -1;
 
   media->pkt = av_packet_alloc();
-  media->frame = av_frame_alloc();
-  media->dst_frame = av_frame_alloc();
+  if (!media->pkt)
+    return NULL;
 
-  media->video_area_width = video_area_width;
-  media->video_area_height = video_area_height;
+  media->frame = av_frame_alloc();
+  if (!media->frame)
+    return NULL;
 
   media->sws_ctx = NULL;
+  ;
 
   return media;
 }
 
-// This opens either the audio or the video codec for later use in decoding
-int media_open_context(Media *media, AVCodecContext **ctx,
-                       enum AVMediaType type) {
+int media_open_contexts(Media *media) {
   int ret;
 
-  const AVCodec *desired_codec;
-  if ((ret = av_find_best_stream(media->fmt_ctx, type, -1, -1, &desired_codec,
-                                 0)) < 0) {
-    printf("Could not find best stream: %s\n", av_err2str(ret));
-    return ret;
-  }
+  for (int i = 0; i < media->fmt_ctx->nb_streams; i++) {
+    AVStream *stream = media->fmt_ctx->streams[i];
 
-  // Set the stream index. Might be useful later on
-  if (type == AVMEDIA_TYPE_VIDEO) {
-    media->video_stream_index = ret;
-  } else {
-    media->audio_stream_index = ret;
-  }
+    const AVCodec *codec = avcodec_find_decoder(stream->codecpar->codec_id);
+    if (!codec) {
+      printf("Could not find decoder for stream\n");
+      return -1;
+    }
 
-  *ctx = avcodec_alloc_context3(desired_codec);
-  if (!ctx) {
-    printf("Could not allocate context");
-    return ret;
-  }
+    AVCodecContext *ctx = avcodec_alloc_context3(codec);
+    if (!ctx) {
+      printf("Could not allocate context");
+      return ret;
+    }
 
-  if ((ret = avcodec_parameters_to_context(
-           *ctx, media->fmt_ctx->streams[ret]->codecpar)) < 0) {
-    printf("Could not copy info to context: %s\n", av_err2str(ret));
-    return ret;
-  }
+    if ((ret = avcodec_parameters_to_context(ctx, stream->codecpar)) < 0) {
+      printf("Could not copy parameters from stream to context: %s\n",
+             av_err2str(ret));
+      return ret;
+    }
 
-  if ((ret = avcodec_open2(*ctx, desired_codec, NULL)) < 0) {
-    printf("Could not open context: %s\n", av_err2str(ret));
-    return ret;
+    if ((ret = avcodec_open2(ctx, codec, NULL)) < 0) {
+      printf("Could not open context: %s\n", av_err2str(ret));
+      return ret;
+    }
+
+    // POSSIBLE ERROR HERE? LOOP INDEX MAY NOT CORRESPOND TO STREAM INDEX??
+    media->ctxs[i] = ctx;
+    media->streams[i] = stream;
+
+    // This is a helper so we can easily find the most useful streams for now
+    if (stream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+      media->video_stream_index = i;
+    } else if (stream->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+      media->audio_stream_index = i;
+    }
+
+    media->nb_streams++;
   }
 
   return 0;
 }
 
-int media_init(Media *media) {
+AVCodecContext *media_get_audio_ctx(Media *media) {
+  if (media->nb_streams <= 0 || media->audio_stream_index == -1) {
+    return NULL;
+  }
+
+  return media->ctxs[media->audio_stream_index];
+}
+
+AVCodecContext *media_get_video_ctx(Media *media) {
+  if (media->nb_streams <= 0 || media->video_stream_index == -1) {
+    return NULL;
+  }
+
+  return media->ctxs[media->video_stream_index];
+}
+
+int media_init(Media *media, int dst_frame_w, int dst_frame_h,
+               enum AVPixelFormat dst_frame_fmt) {
   int ret;
 
   // Setup the format context with the input info
   if ((ret = avformat_open_input(&media->fmt_ctx, media->filename, NULL,
                                  NULL)) < 0) {
-    printf("Could not open source file %s\n", av_err2str(ret));
+    printf("Could not open source file wit libav %s\n", av_err2str(ret));
     return -1;
   }
 
@@ -81,34 +108,22 @@ int media_init(Media *media) {
   av_dump_format(media->fmt_ctx, 0, media->filename, 0);
 
   // Open contexts
-  ret = media_open_context(media, &media->video_codec, AVMEDIA_TYPE_VIDEO);
-  if (ret < 0 && ret != AVERROR_STREAM_NOT_FOUND) {
-    printf("Could not open video context: %s\n", av_err2str(ret));
+  ret = media_open_contexts(media);
+  if (ret < 0) {
+    printf("Could not open media contexts: %s\n", av_err2str(ret));
     return -1;
   }
 
-  ret = media_open_context(media, &media->audio_codec, AVMEDIA_TYPE_AUDIO);
-  if (ret < 0 && ret != AVERROR_STREAM_NOT_FOUND) {
-    printf("Could not open audio context: %s\n", av_err2str(ret));
-    return -1;
-  }
+  AVCodecContext *video_codec = media_get_video_ctx(media);
+  if (video_codec) {
+    media->sws_ctx = sws_getContext(
+        video_codec->width, video_codec->height, video_codec->pix_fmt,
+        dst_frame_w, dst_frame_h, dst_frame_fmt, SWS_BILINEAR, NULL, NULL, 0);
 
-  // Create the conversion context
-  media->dst_frame->width = media->video_area_width;
-  media->dst_frame->height = media->video_area_height;
-  media->dst_frame->format = AV_PIX_FMT_RGBA;
-
-  media->sws_ctx =
-      media->video_codec
-          ? sws_getContext(media->video_codec->width,
-                           media->video_codec->height,
-                           media->video_codec->pix_fmt, media->dst_frame->width,
-                           media->dst_frame->height, media->dst_frame->format,
-                           SWS_BILINEAR, NULL, NULL, 0)
-          : NULL;
-
-  if (!media->sws_ctx) {
-    return -1;
+    if (!media->sws_ctx) {
+      printf("Could not allocate sws context\n");
+      return -1;
+    }
   }
 
   return 0;
@@ -125,8 +140,8 @@ int media_read_frame(Media *media) {
 int media_decode_frames(Media *media) {
   int ret;
   AVCodecContext *codec = media->pkt->stream_index == media->video_stream_index
-                              ? media->video_codec
-                              : media->audio_codec;
+                              ? media_get_video_ctx(media)
+                              : media_get_audio_ctx(media);
 
   if ((ret = avcodec_send_packet(codec, media->pkt)) < 0) {
     return -1;
@@ -136,19 +151,13 @@ int media_decode_frames(Media *media) {
     ret = avcodec_receive_frame(codec, media->frame);
     if (ret < 0) {
       if (ret == AVERROR_EOF || ret == AVERROR(EAGAIN)) {
-        break;
+        return 0;
       }
-
       return -1;
     }
 
     if (media->pkt->stream_index == media->video_stream_index) {
       // Add video frame to video queue
-      if ((ret = sws_scale_frame(media->sws_ctx, media->dst_frame, media->frame)) < 0) {
-
-        printf("Could not scale frame: %s\n", av_err2str(ret));
-      };
-
     } else {
       // Add audio frame to audio queue
     }
@@ -156,23 +165,22 @@ int media_decode_frames(Media *media) {
     av_frame_unref(media->frame);
   }
 
-  int index = media->pkt->stream_index == media->video_stream_index
-                  ? media->video_stream_index
-                  : media->audio_stream_index;
-  av_packet_unref(media->pkt);
-  return index;
+  return 0;
 }
 
 void media_free(Media *media) {
   avformat_close_input(&media->fmt_ctx);
 
   sws_freeContext(media->sws_ctx);
-  avcodec_free_context(&media->video_codec);
-  avcodec_free_context(&media->audio_codec);
+  for (int i = 0; i < media->nb_streams; i++) {
+    avcodec_free_context(&media->ctxs[i]);
+  }
 
   av_packet_free(&media->pkt);
   av_frame_free(&media->frame);
-  av_frame_free(&media->dst_frame);
+
+  if (media->sws_ctx)
+    sws_freeContext(media->sws_ctx);
 
   free(media);
 }
--- media.h
+++ media.h
@@ -9,36 +9,43 @@
 #include <libavutil/imgutils.h>
 #include <libswscale/swscale.h>
 
-#define MAX_FILEPATH_RECORDED 4096
-#define MAX_FILEPATH_SIZE 2048
+#define MAX_FILEPATH_SIZE 1024
+#define MAX_CONTEXTS 3
 
 typedef struct Media {
   char *filename;
-  // Contains info about the image (streams, formats, dimensions, etc.)
-  AVFormatContext *fmt_ctx;
-  AVCodecContext *video_codec, *audio_codec;
 
-  int video_stream_index, audio_stream_index;
-  int video_area_width, video_area_height;
+  AVFormatContext *fmt_ctx;
+
+  int nb_streams, video_stream_index, audio_stream_index;
+  AVCodecContext *ctxs[MAX_CONTEXTS];
+  AVStream *streams[MAX_CONTEXTS];
 
   // This is an auxiliary variable to read all the frames from the container
   // wheter they are audio or video frames.
   AVPacket *pkt;
 
   // Frame for reading purposes
-  AVFrame *frame, *dst_frame;
+  AVFrame *frame;
+
+  // TODO: Here, we will implement storage for audio and video streams
 
   // This is the context that we're gonna use to convert the original pixel
   // format on the frame to the desired format
   struct SwsContext *sws_ctx;
 } Media;
 
-Media *media_alloc(int video_area_width, int video_area_height);
+Media *media_alloc();
 
-int media_open_context(Media *media, AVCodecContext **ctx,
-                       enum AVMediaType type);
-int media_init(Media *media);
+int media_open_contexts(Media *media);
+int media_init(Media *media, int, int, enum AVPixelFormat);
+
+AVCodecContext *media_get_video_ctx(Media *media);
+AVCodecContext *media_get_audio_ctx(Media *media);
+
 int media_read_frame(Media *media);
+int media_seek(Media *media, int64_t target);
+
 int media_decode_frames(Media *media);
 
 void media_free(Media *media);
